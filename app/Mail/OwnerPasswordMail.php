29e8e7d1b490f8c744a46cb55d0447d902289f0f
taka1214 <globar173@gmail.com>
2023-05-16 19:33:53 +0900

    owner mail function

--- app/Mail/OwnerPasswordMail.php
+++ app/Mail/OwnerPasswordMail.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class OwnerPasswordMail extends Mailable
@@ -14,6 +12,7 @@ class OwnerPasswordMail extends Mailable
     use Queueable, SerializesModels;
 
     public $token;
+
     /**
      * Create a new message instance.
      *
@@ -24,42 +23,17 @@ class OwnerPasswordMail extends Mailable
         $this->token = $token;
     }
 
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
     public function build()
     {
-        return $this->view('owner.emails.test');
-    }
-
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
-    {
-        return new Envelope(
-            subject: 'Test Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            view: 'owner.emails.test',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
+        return $this->subject('Your Password Reset Link')
+            ->view('owner.emails.test')
+            ->with([
+                'token' => $this->token,
+            ]);
     }
 }
