584f5001ee5ae00461819441dd917dc3ec3eb127
taka1214 <globar173@gmail.com>
2023-09-08 22:14:48 +0900

    modified-img-html2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ Fine Maison横浜 }}</title>
+        <title>Fine Maison横浜</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
